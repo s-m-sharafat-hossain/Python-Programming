22f04644a7a4d144b631a2149d0bf4ce1374f653
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2025-09-14 07:58:38 +0600

    fg

---
+++ Level 1 Basic/Syntax.py
@@ -0,0 +1,12 @@
+
+
+a= 5
+b=6
+
+if a<b:
+    print("B is getter than A")
+
+# here space is important after condition.
+# if a<b:
+# print("B is getter than A")
+# this is syntax error.
---
+++ Level 1 Basic/Variable.py
@@ -0,0 +1,55 @@
+
+# Creating Variable:
+# -> Python has no command for declaring variable.
+
+x=5
+a="Sharafat Hossain"
+y=5.6
+
+print(x)
+print(y)
+print(a)
+
+
+# Type Casting:
+# -> If I want to specify the type of variable, this can done with casting.
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x,y,z)
+
+
+# Get the Type:
+# -> I can get data type of a variable with the type() function.
+
+x = 5
+y = "Sharafat"
+
+print(type(x))
+print(type(y))
+
+
+# Single or Double quotes "",'' are same.
+# Variable names are case-sensitive.
+# Variable name not allow like this: 2myvar, my-var, my var
+
+# Some variable case:
+# -> Camel Case: myVariableName.
+# -> Pascal Case: MyVariableName.
+# -> Snake Case: my_variable_name.
+
+
+# Multiple Values and Variable
+
+x, y, z = 3, 4, 5
+
+print(x,y,z)
+
+x=y=z=5
+print(x,y,z)
+
+
+
+
