c07db39b56f059ac60c0b99de684b488ea91c1e9
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2026-01-29 08:34:28 +0600

    for Loop

---
+++ 1. Python Basic/8. Loop/ForLoop.py
@@ -0,0 +1,10 @@
+# For Loop in Python:
+# A for loop is used to iterate over a sequence (like a list, tuple, dictionary, set, or string).
+# It allows you to execute a block of code multiple times, once for each item in the sequence.
+# Syntax:
+# for item in sequence:
+#     # code block to be executed
+# Example 1: Iterating through a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
